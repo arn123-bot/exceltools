d39626a2c3411780993fd6ca8b68ed6ebf4d4913
meisan-tech@outlook.com <meisan-tech@outlook.com>
2020-01-16 21:33:02 -0500

    Update Excelcombiner.py

--- Excelcombiner.py
+++ Excelcombiner.py
@@ -5,13 +5,12 @@
 import glob
 import os
 import pandas as pd
-from pandas import ExcelWriter
 from tkinter import *
 from tkinter import filedialog
 
-root = Tk()
+root=Tk()
 def browse():
-    root.filename =  filedialog.askdirectory(initialdir = "/",title = "Select file folder")
+    root.filename=filedialog.askdirectory(initialdir="/",title="Select file folder")
     indir=root.filename
     concator(indir)
 
@@ -30,9 +29,9 @@ def concator(infile):
             print("Step done moving to next")
             df_excel = pd.read_excel(filename, sheet_name=sh_name)
             df_excel.to_excel(writer,sheet_name=sh_name)
-            print("starting:-"+sh_name)
-        print(filename+" is done")
-    print("All files are combained to "+infile+"\concat_output.xlsx")
+            print("starting:-{}".format(sh_name))
+        print("{} is done".format(filename))
+    print("All files are combained to {}/concat_output.xlsx".format(infile))
     writer.save()
 l1=Label(root,text="Select the folder of files:")
 b1=Button(root,text='Browse',command=browse)
@@ -40,10 +39,10 @@ l1.grid(row=0,column=1)
 b1.grid(row=1,column=1)
 root.mainloop()
 
-"""
-Make EXE by :
-   pyinstaller --onefile <<scipt name>>
-"""
+
+#Make EXE by :
+#  pyinstaller --onefile <<scipt name>>
+
 
     
     
